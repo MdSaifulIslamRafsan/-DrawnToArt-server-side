bb8f37762890c0e7d11f1e0207e856903785a83f
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-04-30 20:17:31 +0600

    added deleted mongodb

--- index.js
+++ index.js
@@ -64,6 +64,13 @@ async function run() {
     
     });
 
+    app.delete("/craftItems/:id" , async(req , res ) => {
+      const id = req.params.id;
+      const query = {_id :  new ObjectId(id)}
+      const result = await craftItemsCollection.deleteOne(query);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
